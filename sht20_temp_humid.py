b00de0e4c2c768b9cac0f36e327b46102c785a2d
Noah Hamilton <hamiltonn@usf.edu>
2021-10-29 04:58:34 -0400

    Added initial code for the moisture sensor, didn't work...

--- sht20_temp_humid.py
+++ sht20_temp_humid.py
@@ -1,9 +1,15 @@
 from sensor import SHT20
 from time import sleep
 import smbus2
+import RPi.GPIO as GPIO
 
 # This is the humidy/temperature sensor.
 sht = SHT20(1, 0x40)
+
+# Set input pins for root zone moisture sensor.
+GPIO.setmode(GPIO.BOARD)
+sen0308 = 23
+GPIO.setup(sen0308,GPIO.IN)
 
 # To-Do add in another condition for while loop...
 while True:
@@ -12,5 +18,8 @@ while True:
     humid = data[0]
     temp = data[1]
     
-    print("The temperature is " + str(temp) + " the humidity is " + str(humid) + " %")
+    moist = GPIO.input(sen0308)
+    
+    
+    print(str(temp) + str(humid) + " Moisture:" + str(moist))
     sleep(5)
