d204022de154a52d705d9c511d6452c24d245502
Noah Hamilton <hamiltonn@usf.edu>
2021-10-29 07:57:45 -0400

    Added function to return moisture level to terminal and drive LED.

--- sht20_temp_humid.py
+++ sht20_temp_humid.py
@@ -4,12 +4,32 @@ import smbus2
 import RPi.GPIO as GPIO
 
 # This is the humidy/temperature sensor.
+
 sht = SHT20(1, 0x40)
 
-# Set input pins for root zone moisture sensor.
-GPIO.setmode(GPIO.BOARD)
-sen0308 = 23
+# Set input pins for root zone moisture sensor and LED.
+
+GPIO.setmode(GPIO.BCM)
+sen0308 = 17
 GPIO.setup(sen0308,GPIO.IN)
+LED = 27
+GPIO.setup(LED,GPIO.OUT)
+
+# Create a function to drive LED.
+
+def callback(sen0308):
+    if GPIO.input(sen0308):
+        GPIO.output(27,GPIO.HIGH)
+        return "\nPlant is thirsty!"
+        
+    else:
+        GPIO.output(27,GPIO.LOW)
+        return "\nPlant is hydrated!"
+
+# Return moisture level to terminal and LED.
+
+GPIO.add_event_detect(sen0308, GPIO.BOTH, bouncetime=300)
+GPIO.add_event_callback(sen0308, callback)
 
 # To-Do add in another condition for while loop...
 while True:
@@ -18,8 +38,6 @@ while True:
     humid = data[0]
     temp = data[1]
     
-    moist = GPIO.input(sen0308)
+    print(str(temp) + str(humid) + str(callback(sen0308)))
     
-    
-    print(str(temp) + str(humid) + " Moisture:" + str(moist))
     sleep(5)
