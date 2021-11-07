57cadff341c6bfed02e0ea68c40205dddb529231
Noah Hamilton <hamiltonn@usf.edu>
2021-11-07 17:29:41 -0500

    Added log function for future use. RPi date/time wrong...

--- sht20_temp_humid.py
+++ sht20_temp_humid.py
@@ -1,8 +1,9 @@
 from sensor import SHT20
 from time import sleep
-import smbus2
 import RPi.GPIO as GPIO
-
+from pathlib import Path
+from datetime import datetime
+    
 # This is the humidy/temperature sensor.
 
 sht = SHT20(1, 0x40)
@@ -12,18 +13,18 @@ sht = SHT20(1, 0x40)
 GPIO.setmode(GPIO.BCM)
 sen0308 = 17
 GPIO.setup(sen0308,GPIO.IN)
-LED = 27
-GPIO.setup(LED,GPIO.OUT)
+# LED = 27
+# GPIO.setup(LED,GPIO.OUT)
 
 # Create a function to drive LED.
 
 def callback(sen0308):
     if GPIO.input(sen0308):
-        GPIO.output(27,GPIO.HIGH)
+        #GPIO.output(27,GPIO.HIGH)
         return "\nPlant is thirsty!"
         
     else:
-        GPIO.output(27,GPIO.LOW)
+        #GPIO.output(27,GPIO.LOW)
         return "\nPlant is hydrated!"
 
 # Return moisture level to terminal and LED.
@@ -31,13 +32,23 @@ def callback(sen0308):
 GPIO.add_event_detect(sen0308, GPIO.BOTH, bouncetime=300)
 GPIO.add_event_callback(sen0308, callback)
 
-# To-Do add in another condition for while loop...
-while True:
-    
-    data = sht.all()
-    humid = data[0]
-    temp = data[1]
-    
-    print(str(temp) + str(humid) + str(callback(sen0308)))
-    
-    sleep(5)
+# Create file for data logging.
+
+file = open('eatLog.txt','w+') 
+date = datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p")
+
+# While true loop to run program, use CTRL + C to exit and cleanup pins.
+try:
+
+    while True:
+        with open('eatLog.txt', "a") as log:
+            data = sht.all()
+            humid = data[0]
+            temp = data[1]
+            out = "\n" + date + "\n" + str(temp) + "\n" + str(humid) + str(callback(sen0308)) + "\n"
+            log.write(out)
+        print(out)
+        sleep(5)
+except KeyboardInterrupt:
+    print("------------------\nEAT SYSTEM OFFLINE\n------------------")
+    GPIO.cleanup()
